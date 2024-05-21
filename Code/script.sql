8bd3c003e03cca2ef02959d2bc1dae99879e7c86
Hswwwww <southm123@naver.com>
2024-05-21 17:41:52 +0900

    spring

--- Code/script.sql
+++ Code/script.sql
@@ -10,3 +10,80 @@ create table tblCode(
 
 create sequence seqCode;
 
+
+select * from tblCode;
+
+
+create table tblUser(
+    seq number primary key,
+    name varchar2(30) not null
+);
+
+create sequence seqUser;
+
+create table tblDetail(
+    seq number primary key,
+    email varchar2(100) not null,
+    user_seq number not null references tblUser(seq)
+
+);
+
+create sequence seqDetail;
+
+select * from tblDetail;
+select * from tblUser;
+
+
+
+create table tblInfo (
+	seq number,
+	school varchar2(100) not null,
+	country varchar2(100) not null,
+	
+	constraint info_pk primary key(seq),
+	constraint info_kf foreign key(seq) references tblAddress(seq) 
+);
+
+insert into tblInfo values (1, '媛뺣궓 �룞臾쇳븰援�', '���븳誘쇨뎅');
+insert into tblInfo values (2, '媛뺣궓 �룞臾쇳븰援�', '���븳誘쇨뎅');
+insert into tblInfo values (3, '�돱�슃 �븘移대뜲誘�', '誘멸뎅');
+insert into tblInfo values (4, '�룞寃� �썕�젴�썝', '�씪蹂�');
+insert into tblInfo values (5, '踰좎씠吏� �썕�젴�냼', '以묎뎅');
+
+create table tblMemo (
+	seq number primary key,
+	memo varchar2(500) not null,
+	regdate date default sysdate not null,
+	aseq number not null references tblAddress(seq) 
+);
+
+create sequence seqMemo;
+
+insert into tblMemo values (seqMemo.nextVal, '�븞�뀞�븯�꽭�슂.', default, 1);
+insert into tblMemo values (seqMemo.nextVal, '�궇�뵪媛� 議곌툑 �쓲�젮�슂.', default, 1);
+insert into tblMemo values (seqMemo.nextVal, '�썡�슂�씪�엯�땲�떎.', default, 1);
+insert into tblMemo values (seqMemo.nextVal, '�뒪�봽留� 怨듬��븯怨� �엳�뼱�슂.', default, 1);
+insert into tblMemo values (seqMemo.nextVal, '踰뚯뜥 諛곌� 怨좏뙆�슂.', default, 1);
+insert into tblMemo values (seqMemo.nextVal, '�삤�뒛�룄 �뿴�떖�엳 �궙�옞 以�', default, 2);
+insert into tblMemo values (seqMemo.nextVal, '吏묒궗�빞 諛λ궡�넄�씪!', default, 2);
+insert into tblMemo values (seqMemo.nextVal, '�굪�빟�굪�빟', default, 3);
+insert into tblMemo values (seqMemo.nextVal, '援ш뎄', default, 6);
+insert into tblMemo values (seqMemo.nextVal, '�븯�뒛�쓣 �궇�떎.', default, 9);
+insert into tblMemo values (seqMemo.nextVal, '吏묒씠 醫곸븘�슂.', default, 8);
+
+
+commit;
+
+-- tblAddress : tblInfo
+-- 1 : 1 > join
+-- tblAddress : tblMemo
+-- 1 : N > join
+
+-- tblAddress
+select * from tblAddress;
+
+--tblInfo
+select * from tblInfo;
+
+-- tblMemo
+select * from tblMemo;
--- MyBatisTest/src/main/java/com/test/mybatis/controller/MyBatisController.java
+++ MyBatisTest/src/main/java/com/test/mybatis/controller/MyBatisController.java
@@ -12,7 +12,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.test.mybatis.dao.MyBatisDAO;
+import com.test.mybatis.dto.AddressDTO;
+import com.test.mybatis.dto.DetailDTO;
 import com.test.mybatis.dto.MyBatisDTO;
+import com.test.mybatis.dto.UserDTO;
 
 import lombok.RequiredArgsConstructor;
 
@@ -314,6 +317,80 @@ public class MyBatisController {
 		List<MyBatisDTO> list = dao.m18(name);
 		
 		model.addAttribute("list",list);   
+		
+		return "result";
+	}
+	
+	@GetMapping("/m19.do")
+	public String m19(Model model, UserDTO udto, DetailDTO ddto) {
+		
+		//다중 쿼리 > selectKey
+		//- mapper는 한번에 한개의 SQL만 실행 가능하다.
+		
+		//- tblUser > 회원(기본정보)
+		//- tblDetail > 회원(추가정보)
+		
+		//- m19.do?name=홍길동&email=hong@test.com
+		//- m19.do?name=아무개&email=aaa@test.com
+		//- m19.do?name=강아지
+		
+		
+//		dao.addUser(udto);
+//		
+//		String seq = dao.getSeq();
+//		ddto.setUser_seq(seq);
+//		
+//		dao.addDetail(ddto);
+		
+		dao.addUser(udto);
+		
+		ddto.setUser_seq(udto.getSeq());
+		dao.addDetail(ddto);
+		
+		//System.out.println(udto.getSeq());
+		
+		
+		
+		
+		
+		
+		return "result";
+	}
+	
+	
+	@GetMapping("/m20.do")
+	public String m20(Model model) {
+		
+		//Join 상황
+		//- 1:1
+		
+		//AddressDTO + InfoDTO
+		// select * from tblAddress a inner join tblInfo i on a.seq = i.seq
+		
+		//1. AddressDTO + InfoDTO = DTO (통합)
+		//2. AddressDTO(InfoDTO)
+		
+		List<AddressDTO> jlist = dao.m20();
+		model.addAttribute("jlist",jlist);
+		
+		return "result";
+	}
+	
+	@GetMapping("/m21.do")
+	public String m21(Model model) {
+		List<AddressDTO> jlist = dao.m21();
+		model.addAttribute("jlist",jlist);
+		
+		return "result";
+	}
+	
+	@GetMapping("/m22.do")
+	public String m22(Model model) {
+	
+		// 1:N
+		// tblAddress + tblMemo > 한사람이 여러개의 메모작성 
+		List<AddressDTO> mlist = dao.m22();
+		model.addAttribute("mlist",mlist);
 		
 		return "result";
 	}
