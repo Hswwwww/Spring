42f3ea8977caf4f251cad561d370ecee197bfb96
Hswwwww <southm123@naver.com>
2024-05-27 15:33:16 +0900

    spring security Board

---
+++ SecurityBoardTest/src/main/java/com/test/java/dto/MemberDTO.java
@@ -0,0 +1,27 @@
+package com.test.java.dto;
+
+import java.util.List;
+
+import com.test.java.dto.AuthDTO;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class MemberDTO {
+
+	
+	private String memberid;
+	private String memberpw;
+	private String membername;
+	private String enabled;
+	private String email;
+	private String gender;
+	private String regdate;
+
+	private List<AuthDTO> authlist;
+	private String auth; //회원 가입용
+}
